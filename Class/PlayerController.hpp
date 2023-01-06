3d22bb15e4684c94cdc93b10937e87777d10e10b
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2023-01-07 02:04:42 +0800

    Changed format method

--- Class/PlayerController.hpp
+++ Class/PlayerController.hpp
@@ -46,13 +46,20 @@ public:
     static void printAllPlayersInfo(std::list<PlayerController> playerControllers, Utils* utils = nullptr) {
         std::list<PlayerController>::iterator iterator = playerControllers.begin();
 
-        const char separator = ' ';
+        const char separator = '-';
         const int nameWidth = 15;
 
-        std::cout << std::left << std::setw(20) << std::setfill(separator) << (utils ? utils->str("Nickname", "玩家昵称") : "玩家昵称");
+        std::cout << std::format("{:25}{:15}{:15}{:10}\n",
+            utils ? utils->str("Nickname", "玩家昵称") : "玩家昵称",
+            utils ? utils->str("Rolename", "角色") : "角色",
+            utils ? utils->str("KilledThisRound", "本轮杀过人") : "本轮杀过人",
+            utils ? utils->str("DeadTime", "死亡时间") : "死亡时间");
+        /*
+        std::cout << std::left << std::setw(25) << std::setfill(separator) << (utils ? utils->str("Nickname", "玩家昵称") : "玩家昵称");
         std::cout << std::left << std::setw(15) << std::setfill(separator) << (utils ? utils->str("Rolename", "角色") : "角色");
-        std::cout << std::left << std::setw(10) << std::setfill(separator) << (utils ? utils->str("KilledThisRound", "本轮杀过人") : "本轮杀过人");
-
+        std::cout << std::left << std::setw(15) << std::setfill(separator) << (utils ? utils->str("KilledThisRound", "本轮杀过人") : "本轮杀过人");
+        std::cout << std::left << std::setw(10) << std::setfill(separator) << (utils ? utils->str("DeadTime", "死亡时间") : "死亡时间");
+        */
         //std::cout << std::left << std::setfill(separator) << (utils ? utils->str("Pos", "坐标") : "坐标");
         std::cout << '\n';
 
@@ -62,10 +69,21 @@ public:
             }
 
             bool killedThisRound = (*iterator).b_hasKilledThisRound;
+            int deathTime = (*iterator).i_timeOfDeath;
 
-            std::cout << std::left << std::setw(20) << std::setfill(separator) << (*iterator).nickname;
+            std::cout << std::format("{:25}{:15}{:15}{:10}",
+                (*iterator).nickname,
+                (*iterator).roleName,
+                killedThisRound ? (utils ? utils->str("Yes", "是") : "是") : "",
+                (*iterator).i_timeOfDeath ? std::to_string(((*iterator).i_timeOfDeath)) : ""
+            );
+            /*
+            std::cout << std::left << std::setw(25) << std::setfill(separator) << (*iterator).nickname;
             std::cout << std::left << std::setw(15) << std::setfill(separator) << (*iterator).roleName;
-            std::cout << std::left << std::setw(10) << std::setfill(separator) << (killedThisRound ? (utils ? utils->str("Yes", "是") : "是") : "");
+            std::cout << std::left << std::setw(15) << std::setfill(separator) << (killedThisRound ? (utils ? utils->str("Yes", "是") : "是") : "");
+            std::cout << std::left << std::setw(10) << std::setfill(separator);
+            */
+
             //    std::cout << std::left << std::setfill(separator) << "(" << (*iterator).pos.x << ", " << (*iterator).pos.y << ")";
             std::cout << '\n';
         }
