720ba154e4d0da98ab8a90f4b5232e7effdf3706
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2023-01-15 22:06:13 +0800

    TODO

--- Class/DataUpdater.hpp
+++ Class/DataUpdater.hpp
@@ -110,6 +110,7 @@ private:
             return;
         }
 
+        //TODO: 穿墙没有正常工作, 另外需要在每次游戏开始的时候重置修改值
         hack.removeFogOfWar(&localPlayer->playerController);
         hack.noclip(&localPlayer->playerController);//TODO: Bugged 会导致游戏崩溃
         hack.speedHack(localPlayer);
