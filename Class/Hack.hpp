e27ebc18aab394a3fd4df30b4e4f0b0f0af04301
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2023-01-16 13:17:38 +0800

    修复了移速hack

--- Class/Hack.hpp
+++ Class/Hack.hpp
@@ -113,43 +113,69 @@ public:
         //开启穿墙
         if (localPlayerController && localPlayerController->b_isLocal) {
             if (this->client && this->client->hackSettings) {
-             enableNoclip(localPlayerController,
-                 this->client->hackSettings->guiSettings.b_alwaysEnableNoclip || this->client->hackSettings->tempEnableNoclip);
+                enableNoclip(localPlayerController,
+                    this->client->hackSettings->guiSettings.b_alwaysEnableNoclip || this->client->hackSettings->tempEnableNoclip);
             }
         }
     }
 
-    //TODO: reset player's speed when game finished
-    //TODO: reset player's speed when disabled speedHack
-    void speedHack(LocalPlayer* localPlayer) {
-        if (!this->client->hackSettings->guiSettings.b_enableSpeedHack) {
-            return;
+    bool speedHack(LocalPlayer* localPlayer) {
+        //目标速度
+        float targetSpeed = -1.0f;
+
+        //检查是否启用移速hack
+        ActivationState state = utils.shouldActivateOnce(this->client->hackSettings->guiSettings.b_enableSpeedHack, &this->b_enabledSpeedHackLastTime);
+
+        switch (state) {
+            case SHOULD_DEACTIVATE_NOW:
+                //TODO: 用户关闭了speedhack，恢复正常移速
+                targetSpeed = this->client->hackSettings->gameOriginalData.f_baseMovementSpeed;
+                break;
+
+                //speedhack没有被切换开关
+            case IDLE_DO_NOTHING:
+                //没有开启
+                if (!this->client->hackSettings->guiSettings.b_enableSpeedHack) {
+                    return false;
+                }
+                else {
+                    //继续向下执行逻辑
+                }
+
+                //用户刚刚开启speedhack
+            case SHOULD_ACTIVATE_NOW:
+                //TODO: 检查启用状态下当前目标速度是否更新
+                targetSpeed = this->client->hackSettings->guiSettings.f_movementSpeed;
+
+                //和上一次设置的速度一样，无变化，直接返回
+                if (this->lastTimeSetMovementSpeed == this->client->hackSettings->guiSettings.f_movementSpeed) {
+                    return false;
+                }
+                break;
         }
 
-        static float targetSpeed = this->client->hackSettings->guiSettings.f_movementSpeed;
-
-        //目标速度未变化，直接返回
-        if (targetSpeed == this->client->hackSettings->guiSettings.f_movementSpeed) {
-            return;
-        }
+        //修改移速逻辑从这里开始       
 
         if (targetSpeed < 0) {
             targetSpeed = this->client->hackSettings->gameOriginalData.f_baseMovementSpeed;
-            this->client->hackSettings->guiSettings.f_movementSpeed = targetSpeed;
         }
-        else {
-            Memory* memory = this->client->getMemory();
-            //开启穿墙
-            if (localPlayer) {
-                std::vector<int64_t> offsets = {
-                    Offsets::LocalPlayer::ptr_Class,
-                    Offsets::LocalPlayer::Class::ptr_staticFields,
-                    Offsets::LocalPlayer::Class::StaticField::f_movementSpeed
-                };
-                int64_t movementSpeed_addr = memory->FindPointer(localPlayer->address, offsets);
 
-                memory->write_mem<float>(movementSpeed_addr, targetSpeed);
-            }
+        Memory* memory = this->client->getMemory();
+        //开启穿墙
+        if (localPlayer) {
+            std::vector<int64_t> offsets = {
+                Offsets::LocalPlayer::ptr_Class,
+                Offsets::LocalPlayer::Class::ptr_staticFields,
+                Offsets::LocalPlayer::Class::StaticField::f_movementSpeed
+            };
+            int64_t movementSpeed_addr = memory->FindPointer(localPlayer->address, offsets);
+
+            memory->write_mem<float>(movementSpeed_addr, targetSpeed);
+
+            //更新GUI显示的设置速度
+            this->client->hackSettings->guiSettings.f_movementSpeed = targetSpeed;
+            //更新上一次设置的速度
+            this->lastTimeSetMovementSpeed = targetSpeed;
         }
     }
 
@@ -158,4 +184,9 @@ private:
 
     bool b_hasDisabledFOW = false;
     bool b_hasEnabledNoclip = false;
+
+    //上一次设置移速hack是否开启
+    bool b_enabledSpeedHackLastTime = false;
+    //上一次设置的玩家移速
+    float lastTimeSetMovementSpeed = -1;
 };
